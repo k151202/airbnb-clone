ce3a2484b9ec943c424f955e0fd1ad56387354ae
khan <k151202@naver.com>
2021-03-14 23:42:16 +0900

    prepare for deploy

---
+++ users/models.py
@@ -0,0 +1,102 @@
+import uuid
+from django.utils.translation import gettext_lazy
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.core.mail import send_mail
+from django.utils.html import strip_tags
+from django.shortcuts import reverse
+from django.template.loader import render_to_string
+from core import managers as core_managers
+
+# Create your models here.
+
+
+class User(AbstractUser):
+
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW"),
+    )
+
+    LOGIN_EMAIL = "email"
+    LOGIN_GITHUB = "github"
+    LOGIN_KAKAO = "kakao"
+
+    LOGIN_CHOICES = (
+        (LOGIN_EMAIL, "email"),
+        (LOGIN_GITHUB, "github"),
+        (LOGIN_KAKAO, "kakao"),
+    )
+
+    avatar = models.ImageField(upload_to="avatars", null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES,
+        max_length=2,
+        null=True,
+        blank=True,
+        default=LANGUAGE_KOREAN,
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES,
+        max_length=3,
+        null=True,
+        blank=True,
+        default=CURRENCY_KRW,
+    )
+    superhost = models.BooleanField(default=False)
+    email_verified = models.BooleanField(default=False)
+    email_secret = models.CharField(max_length=120, default="", blank=True)
+    login_method = models.CharField(
+        max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
+    )
+    objects = core_managers.CustomModelManager()
+
+    def get_absolute_url(self):
+        return reverse("users:profile", kwargs={"pk": self.pk})
+
+    def verify_email(self):
+        if self.email_verified is False:
+            secret = uuid.uuid4().hex[:20]
+            self.email_secret = secret
+            html_message = render_to_string(
+                "emails/verify_email.html", {"secret": secret}
+            )
+            send_mail(
+                "Verify Airbnb Account",
+                strip_tags(html_message),
+                settings.EMAIL_FROM,
+                [self.email],
+                fail_silently=False,
+                html_message=html_message,
+            )
+            self.save()
+        return
